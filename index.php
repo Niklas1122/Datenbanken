09a80932b6ca1bc7cc12b334a3b0165b7160696a
Bedirhan Cakir <b.cakir02@hotmail.com>
2026-04-20 11:19:38 +0200

    Positionierung Login

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <h1> RennradSV </h1>
 <p> Hier findest du alle Informationen zu unserem Rennradverein. </p>
 <div> 
+    style=absolute; top: 0px; left: 0px; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: center;
     Anmeldename: <input type="text" name="username"> <br>
     Passwort: <input type="password" name="password"> <br> 
 </div>
