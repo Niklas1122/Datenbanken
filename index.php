6a9847e08f12a35552c2ac2a72846e9111776b11
Bedirhan Cakir <b.cakir02@hotmail.com>
2026-04-20 08:27:55 +0200

    bbb

--- index.php
+++ index.php
@@ -12,13 +12,4 @@
      Hier findest du alle Informationen zu unserem Rennradverein.
 </p>
 </body>
-=======
-                <style>
-            <h1> {
-                color: blue;
-            }
-        </style>
-<h1> RennradSV </h1>
-    </body>
->>>>>>> ba45580e1902f554eb1a0235cc9ce0e1ad72b33f
     </html>
